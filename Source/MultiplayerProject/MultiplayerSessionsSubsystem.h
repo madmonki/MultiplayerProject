a806b47e012319fbf19fb4949d0b49437b0d94a2
madmonki <emozdemir@protonmail.com>
2025-02-02 14:15:14 +0300

    - MSS

---
+++ Source/MultiplayerProject/MultiplayerSessionsSubsystem.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "MultiplayerSessionsSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERPROJECT_API UMultiplayerSessionsSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+public:
+	UMultiplayerSessionsSubsystem();
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+	void PrintString(const FString& String);
+};
